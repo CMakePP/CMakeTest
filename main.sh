0c8a64176fe75070e00d87b3231282623958c41b
Branden Butler <bwtbutler@hotmail.com>
2020-07-09 13:49:04 -0500

    Remove unnecessary commented code in CMakeLists.txt and main.sh

--- main.sh
+++ main.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 pushd . > /dev/null
-#rm -rf ~/git/CMakeTest/build
-#mkdir ~/git/CMakeTest/build
-cd ~/git/CMakeTest/build
+
+mkdir -p build
+cd build
 #for i in {1..500}
 #do
 #        echo $i
 #        cmake .. 2> /dev/null > /dev/null
 #done
+
 cmake ..
 ret=$?
 
