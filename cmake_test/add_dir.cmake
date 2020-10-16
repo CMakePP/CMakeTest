e6958a9e26e98a1f5a83ed277d325828a0524a7f
Branden Butler <bwtbutler@hotmail.com>
2020-10-16 14:15:20 -0500

    Finish migrating add_dir to cmake_tests from tests/CMakeLists.txt

--- cmake_test/add_dir.cmake
+++ cmake_test/add_dir.cmake
@@ -1,23 +1,24 @@
-include_guard()
+\include_guard()
 set(_ct_add_dir_file_dir "${CMAKE_CURRENT_LIST_DIR}")
 
 
 function(ct_add_dir dir)
-    file(GLOB _atd_files  "${dir}/*.cmake") #"${dir}/CMakeLists.txt")
-    message("Files: ${_atd_files}")
+    get_filename_component(_atd_abs_test_dir "${dir}" REALPATH)
+    file(GLOB_RECURSE _atd_files LIST_DIRECTORIES TRUE FOLLOW_SYMLINKS "${_atd_abs_test_dir}/*.cmake") #Recurse over target dir to find all cmake files
+
     foreach(_atd_test_file ${_atd_files})
-        file(RELATIVE_PATH _atd_rel_path 
-        #include(${CMAKE_CURRENT_LIST_DIR}/${testi}.cmake)
-        configure_file("${_ct_add_dir_file_dir}/template_lists.txt" "${CMAKE_CURRENT_BINARY_DIR}/tests/${testi}/src/CMakeLists.txt" @ONLY)
+        file(RELATIVE_PATH _atd_rel_path "${_atd_abs_test_dir}" "${_atd_test_file}") #Find rel path so we don't end up with insanely long paths under test folders
+
+        configure_file("${_ct_add_dir_file_dir}/template_lists.txt" "${CMAKE_CURRENT_BINARY_DIR}/tests/${_atd_rel_path}/src/CMakeLists.txt" @ONLY) #Fill in boilerplate, copy to build dir
         add_test(
         NAME
-            ${testi}
+            ${_atd_rel_path}
         COMMAND
             "${CMAKE_CTEST_COMMAND}"
-                --build-and-test "${CMAKE_CURRENT_BINARY_DIR}/tests/${testi}/src"
-                                 "${CMAKE_CURRENT_BINARY_DIR}/tests/${testi}"
+                --build-and-test "${CMAKE_CURRENT_BINARY_DIR}/tests/${_atd_rel_path}/src"
+                                 "${CMAKE_CURRENT_BINARY_DIR}/tests/${_atd_rel_path}"
                 --build-generator "${CMAKE_GENERATOR}"
                 --test-command "${CMAKE_CTEST_COMMAND}"
-        )
+        ) #Register boilerplate with CTest
     endforeach()
 endfunction()
