049596094c8df4caf4b68f7c3d63748101d7489f
Branden Butler <bwtbutler@hotmail.com>
2022-04-09 22:15:29 -0500

    Remove all code in add_test and exec_tests referencing old internal representation

--- cmake_test/exec_tests.cmake
+++ cmake_test/exec_tests.cmake
@@ -22,47 +22,41 @@ function(ct_exec_tests)
 
     message(STATUS "Executing tests")
 
-    cpp_set_global(CMAKETEST_TESTS_DID_PASS "TRUE") #Default to true and set to false once one does not pass
+    #Default to true and set to false once one does not pass
+    cpp_set_global(CMAKETEST_TESTS_DID_PASS "TRUE")
 
     # Add general exception handler that catches all exceptions
     ct_register_exception_handler()
 
-    cpp_get_global(_et_tests "CMAKETEST_TESTS")
     cpp_get_global(_et_instances "CMAKETEST_TEST_INSTANCES")
 
 
-    list(LENGTH "${_et_tests}" _et_num_tests)
-    foreach(_et_iterator RANGE "${_et_num_tests}")
-        list(GET _et_tests "${_et_iterator}" _et_curr_test)
-        list(GET _et_instances "${_et_iterator}" _et_curr_instance)
-
+    foreach(_et_curr_instance IN LISTS _et_instances)
+        CTExecutionUnit(GET "${_et_curr_instance}" _et_curr_test_id name)
 
 
         #Set the fully qualified identifier for this test, used later for exception tracking and section/subsection execution
         cpp_set_global("CT_CURRENT_EXECUTION_UNIT_INSTANCE" "${_et_curr_instance}")
-        cpp_set_global("CT_CURRENT_EXECUTION_UNIT" "${_et_curr_test}")
-        #cpp_get_global(_et_friendly_name "CMAKETEST_TEST_${_et_curr_test}_FRIENDLY_NAME")
-        #cpp_get_global(_et_expect_fail "CMAKETEST_TEST_${_et_curr_test}_EXPECTFAIL")
-        #cpp_get_global(_et_print_length "CMAKETEST_TEST_${_et_curr_test}_PRINT_LENGTH")
+
         CTExecutionUnit(GET "${_et_curr_instance}" _et_friendly_name friendly_name)
         CTExecutionUnit(GET "${_et_curr_instance}" _et_expect_fail expect_fail)
         CTExecutionUnit(GET "${_et_curr_instance}" _et_print_length print_length)
-        CTExecutionUnit(GET "${_et_curr_instance}" _et_exceptions exceptions)
 
         cpp_set_global("CMAKETEST_SECTION_DEPTH" 0)
 
 
+        #Execute test
+        file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${_et_curr_test_id}/${_et_curr_test_id}.cmake" "${_et_curr_test_id}()")
+        include("${CMAKE_CURRENT_BINARY_DIR}/${_et_curr_test_id}/${_et_curr_test_id}.cmake")
 
-        file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${_et_curr_test}/${_et_curr_test}.cmake" "${_et_curr_test}()")
-        include("${CMAKE_CURRENT_BINARY_DIR}/${_et_curr_test}/${_et_curr_test}.cmake")
-
-        CTExecutionUnit(to_string "${_et_curr_instance}" ret)
-        message("${ret}")
+        CTExecutionUnit(GET "${_et_curr_instance}" _et_exceptions exceptions)
 
         if(_et_expect_fail)
             if("${_et_exceptions}" STREQUAL "")
                 message("${CT_BoldRed}Test named \"${_et_friendly_name}\" was expected to fail but did not throw any exceptions or errors.${CT_ColorReset}")
-                cpp_set_global(CMAKETEST_TESTS_DID_PASS "FALSE") #At least one test failed, so we will inform the caller that not all tests passed.
+
+                #At least one test failed, so we will inform the caller that not all tests passed.
+                cpp_set_global(CMAKETEST_TESTS_DID_PASS "FALSE")
                 set(_et_test_fail "TRUE")
             endif()
         else()
@@ -72,7 +66,9 @@ function(ct_exec_tests)
                     message("${CT_BoldRed}Test named \"${_et_friendly_name}\" raised exception:")
                     message("${_et_exc}${CT_ColorReset}")
                 endforeach()
-                cpp_set_global(CMAKETEST_TESTS_DID_PASS "FALSE") #At least one test failed, so we will inform the caller that not all tests passed.
+
+                #At least one test failed, so we will inform the caller that not all tests passed.
+                cpp_set_global(CMAKETEST_TESTS_DID_PASS "FALSE")
                 set(_et_test_fail "TRUE")
             endif()
         endif()
@@ -86,13 +82,11 @@ function(ct_exec_tests)
 
 
         #Only execute second time if sections detected
-        #cpp_get_global(_et_has_sections "CMAKETEST_TEST_${_et_curr_test}_SECTIONS")
         CTExecutionUnit(GET "${_et_curr_instance}" _et_section_map children)
         cpp_map(KEYS "${_et_section_map}" _et_has_sections)
 	if(NOT _et_has_sections STREQUAL "")
-            cpp_set_global("CMAKETEST_TEST_${_et_curr_test}_EXECUTE_SECTIONS" TRUE)
             CTExecutionUnit(SET "${_et_curr_instance}" execute_sections TRUE)
-            include("${CMAKE_CURRENT_BINARY_DIR}/${_et_curr_test}/${_et_curr_test}.cmake")
+            include("${CMAKE_CURRENT_BINARY_DIR}/${_et_curr_test_id}/${_et_curr_test_id}.cmake")
         endif()
 
     endforeach()
