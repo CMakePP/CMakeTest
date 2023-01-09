d78cf7f1829e2f06cd4860291442a517b12665f1
Branden Butler <bwtbutler@hotmail.com>
2023-01-09 15:18:12 -0600

    Add test for basic EXPECTFAIL usage

---
+++ tests/expectfail.cmake
@@ -0,0 +1,16 @@
+include(cmake_test/cmake_test)
+
+#[[[
+# This test just ensures that throwing a FATAL_ERROR
+# in an EXPECTFAIL test will allow the test to succeed.
+#]]
+ct_add_test(NAME "make_sure_function_fails" EXPECTFAIL)
+function("${make_sure_function_fails}")
+
+    function(failing_fxn)
+        message(FATAL_ERROR "I have erred.")
+    endfunction()
+
+    failing_fxn()
+
+endfunction()
