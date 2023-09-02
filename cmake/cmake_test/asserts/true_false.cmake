c2a9dc406d5bc289168d9624817a2c63411e3417
Branden Butler <bwtbutler@hotmail.com>
2023-09-01 21:38:36 -0500

    Fix assert_true and assert_false

--- cmake/cmake_test/asserts/true_false.cmake
+++ cmake/cmake_test/asserts/true_false.cmake
@@ -16,13 +16,28 @@ include_guard()
 
 #[[[
 # Asserts that the provided variable is true.
+# :code:`var` must be a non-empty string,
+# otherwise an :code:`ASSERTION_FAILED`
+# exception is raised.
 #
-# :param var: The boolean to check for truthiness.
-# :type var: bool*
+# :param var: The variable to check for truthiness.
+# :type var: str*
 #]]
 function(ct_assert_true _at_var)
-    cpp_assert_signature("${ARGV}" bool*)
-    if(NOT "${_at_var}")
+    cpp_assert_signature("${ARGV}" str*)
+
+    # Check separately for empty string
+    # since it will blow up the next if statement
+    # otherwise
+    if (_at_var STREQUAL "")
+        cpp_raise(
+            ASSERTION_FAILED
+            "ct_assert_true() given empty string as parameter"
+        )
+    endif()
+    # Unquoted because quoted strings are always false
+    # unless they are a true constant.
+    if(NOT ${_at_var})
         cpp_raise(
             ASSERTION_FAILED
             "${_at_var} is not true."
@@ -33,11 +48,24 @@ endfunction()
 #[[[
 # Asserts that the provided variable is false.
 #
-# :param var: The boolean variable to check for falseness.
-# :type var: bool*
+# :param var: The variable to check for falseness.
+# :type var: str*
 #]]
 function(ct_assert_false _af_var)
-    cpp_assert_signature("${ARGV}" bool*)
+    cpp_assert_signature("${ARGV}" str*)
+
+    # Check separately for empty string
+    # since it will blow up the next if statement
+    # otherwise
+    if (_at_var STREQUAL "")
+        cpp_raise(
+            ASSERTION_FAILED
+            "ct_assert_true() given empty string as parameter"
+        )
+    endif()
+
+    # Unquoted because quoted strings are always false
+    # unless they are a true constant
     if(${_af_var})
         cpp_raise(
             ASSERTION_FAILED
