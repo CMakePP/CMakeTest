2fe5a80d7533f41157b69a08d6ef6faf2998933b
Ryan M. Richard <rrichard@ameslab.gov>
2025-06-02 10:37:08 -0500

    try without policy

--- tests/cmake_test/asserts/library_exists.cmake
+++ tests/cmake_test/asserts/library_exists.cmake
@@ -1,5 +1,4 @@
 include(cmake_test/cmake_test)
-cmake_policy(SET CMP0002 OLD) #Allow duplicate targets by overriding, needed for multiple tests that work with the same target
 
 ct_add_test(NAME assert_library_exists)
 function(${assert_library_exists})
