619da97ddbde0ce200f397176a7b8ac5550d5e73
Ryan M. Richard <rrichard@ameslab.gov>
2025-06-02 14:48:26 -0500

    finer-grained target creation

--- tests/cmake_test/asserts/target_exists.cmake
+++ tests/cmake_test/asserts/target_exists.cmake
@@ -1,23 +1,26 @@
 include(cmake_test/cmake_test)
-cmake_policy(SET CMP0002 OLD) #Allow duplicate targets by overriding, needed for multiple tests that work with the same target
-
 
 ct_add_test(NAME assert_target_exists)
 function(${assert_target_exists})
     include(cmake_test/asserts/target_exists)
-    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/a.c" "")
-    add_library(my_lib "${CMAKE_CURRENT_BINARY_DIR}/a.c")
 
     ct_add_section(NAME test_target_exists)
     function(${test_target_exists})
-        add_custom_target(my_target ALL)
-        ct_assert_target_exists(my_target)
+        set(target_name "target_${test_target_exists}")
+
+        file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/a.c" "")
+        add_library("${target_name}" "${CMAKE_CURRENT_BINARY_DIR}/a.c")
+        ct_assert_target_exists("${target_name}")
     endfunction()
 
     ct_add_section(NAME test_target_does_not_exist EXPECTFAIL)
     function(${test_target_does_not_exist})
+        set(target_name "target_${test_target_does_not_exists}")
+        file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/a.c" "")
+        add_library("${target_name}" "${CMAKE_CURRENT_BINARY_DIR}/a.c")
         ct_assert_target_exists(non_existant_target)
     endfunction()
+
 endfunction()
 
 ct_add_test(NAME assert_target_does_not_exist)
@@ -26,8 +29,9 @@ function(${assert_target_does_not_exist})
 
     ct_add_section(NAME test_target_exists EXPECTFAIL)
     function(${test_target_exists})
-        add_custom_target(my_target ALL)
-        ct_assert_target_does_not_exist(my_target)
+        set(target_name "target_${test_target_exist}")
+        add_custom_target("${target_name}" ALL)
+        ct_assert_target_does_not_exist("${target_name}")
     endfunction()
 
     ct_add_section(NAME test_target_does_not_exist)
--- tests/cmake_test/asserts/target_has_property.cmake
+++ tests/cmake_test/asserts/target_has_property.cmake
@@ -1,5 +1,4 @@
 include(cmake_test/cmake_test)
-cmake_policy(SET CMP0002 OLD) #Allow duplicate targets by overriding, needed for multiple tests that work with the same target
 
 ct_add_test(NAME assert_target_has_property)
 function(${assert_target_has_property})
@@ -7,23 +6,13 @@ function(${assert_target_has_property})
 
     ct_add_section(NAME test_target_exists)
     function(${test_target_exists})
+        set(target_name "target_${assert_target_has_property}")
         # Create target and add property
-        add_custom_target(my_target ALL)
-        ct_assert_target_exists(my_target)
-        set_target_properties(
-            my_target
-            PROPERTIES property_1 value_1
-        )
+        add_custom_target("${target_name}" ALL)
+        ct_assert_target_exists("${target_name}")
+        set_target_properties("${target_name}" PROPERTIES property_1 value_1)
 
-        ct_add_section(NAME test_target_has_property)
-        function(${test_target_has_property})
-            ct_assert_target_has_property(my_target property_1)
-        endfunction()
-
-        ct_add_section(NAME test_target_does_not_have_property EXPECTFAIL)
-        function(${test_target_does_not_have_property})
-            ct_assert_target_has_property(my_target non_existant_property)
-        endfunction()
+        ct_assert_target_has_property("${target_name}" property_1)
     endfunction()
 
     ct_add_section(NAME test_target_does_not_exist EXPECTFAIL)
@@ -38,23 +27,19 @@ function(${assert_target_does_not_have_property})
 
     ct_add_section(NAME test_target_exists)
     function(${test_target_exists})
+        set(target_name "target_${test_target_exists}")
+
         # Create target and add property
-        add_custom_target(my_target ALL)
-        ct_assert_target_exists(my_target)
+        add_custom_target("${target_name}" ALL)
+        ct_assert_target_exists("${target_name}")
         set_target_properties(
-            my_target
+            "${target_name}"
             PROPERTIES property_1 value_1
         )
 
-        ct_add_section(NAME test_target_has_property EXPECTFAIL)
-        function(${test_target_has_property})
-            ct_assert_target_does_not_have_property(my_target property_1)
-        endfunction()
-
-        ct_add_section(NAME test_target_does_not_have_property)
-        function(${test_target_does_not_have_property})
-            ct_assert_target_does_not_have_property(my_target non_existant_property)
-        endfunction()
+        ct_assert_target_does_not_have_property(
+            "${target_name}" non_existant_property
+        )
     endfunction()
 
     ct_add_section(NAME test_target_does_not_exist EXPECTFAIL)
